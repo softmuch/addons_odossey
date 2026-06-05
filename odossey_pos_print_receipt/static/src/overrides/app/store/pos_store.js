255f4b88ef1c27d7931e8c30086a23268118d2da
yamil <yamilgiralda@gmail.com>
2026-06-05 21:46:58 +0200

    [UPD] odossey_pos_print_receipt

--- odossey_pos_print_receipt/static/src/overrides/app/store/pos_store.js
+++ odossey_pos_print_receipt/static/src/overrides/app/store/pos_store.js
@@ -4,8 +4,10 @@ import { PosStore } from "@point_of_sale/app/store/pos_store";
 
 patch(PosStore.prototype, {
     async printReceipt({ basic = false, order = this.get_order() } = {}) {
-        const result = super.printReceipt(...arguments);
-        await this.data.write("pos.order", [order.id], { nb_print: 0 });
+        const result = await super.printReceipt(...arguments);
+        if (Number.isInteger(order.id)) {
+            await this.data.write("pos.order", [order.id], { nb_print: 0 });
+        }
         return result;
     },
 });
