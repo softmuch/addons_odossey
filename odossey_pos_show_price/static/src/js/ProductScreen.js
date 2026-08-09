5162854b5e0030bd90655e56a960358301a570d6
yamil <yamilgiralda@gmail.com>
2026-08-09 13:43:34 +0200

    [UPD] pricelist odossey_pos_show_price

--- odossey_pos_show_price/static/src/js/ProductScreen.js
+++ odossey_pos_show_price/static/src/js/ProductScreen.js
@@ -3,7 +3,14 @@ import { ProductScreen } from "@point_of_sale/app/screens/product_screen/product
 
 patch(ProductScreen.prototype, {
     getProductPrice(product) {
-        const formattedUnitPrice = product.displayPriceUnit;
+        const order = this.pos.getOrder();
+        const pricelist = order?.pricelist_id || this.pos.config.pricelist_id || false;
+        const taxDetails = product.getTaxDetails({ pricelist });
+        const price =
+            this.pos.config.iface_tax_included === "total"
+                ? taxDetails.total_included
+                : taxDetails.total_excluded;
+        const formattedUnitPrice = this.env.utils.formatCurrency(price);
         if (product.to_weight) {
             return `${formattedUnitPrice}/${product.uom_id.name}`;
         }
