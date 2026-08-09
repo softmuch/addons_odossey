e1084f4673b0a3599b113ab893404b1adf889632
yamil <yamilgiralda@gmail.com>
2026-08-09 10:25:58 +0200

    [UPD] price in cards

---
+++ odossey_pos_show_price/__manifest__.py
@@ -0,0 +1,21 @@
+{
+    'name': 'Odossey || POS Show Price',
+    'version': '18.0.1.0.0',
+    'category': 'Sales/Point of Sale',
+    'author': 'Odossey',
+    'website': 'odossey.com',
+    'summary': 'Show product price on the ProductScreen grid cards',
+    'depends': ['point_of_sale'],
+    'data': [],
+    'installable': True,
+    'application': False,
+    'license': 'OPL-1',
+    'assets': {
+        'point_of_sale._assets_pos': [
+            'odossey_pos_show_price/static/src/js/ProductCard.js',
+            'odossey_pos_show_price/static/src/xml/ProductCard.xml',
+            'odossey_pos_show_price/static/src/xml/ProductScreen.xml',
+            'odossey_pos_show_price/static/src/scss/product_card.scss',
+        ],
+    },
+}
---
+++ odossey_pos_show_price/static/src/js/ProductCard.js
@@ -0,0 +1,6 @@
+import { patch } from "@web/core/utils/patch";
+import { ProductCard } from "@point_of_sale/app/generic_components/product_card/product_card";
+
+patch(ProductCard.props, {
+    price: { type: String, optional: true },
+});
